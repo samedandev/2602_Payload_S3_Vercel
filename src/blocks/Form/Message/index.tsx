1a56c5a7f18efffe4622fba7d06662f53bfb7384
John Doe <johndoe@example.com>
2026-02-12 00:06:22 +0100

    form Message error

--- src/blocks/Form/Message/index.tsx
+++ src/blocks/Form/Message/index.tsx
@@ -7,7 +7,8 @@ import { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
 export const Message: React.FC<{ message: SerializedEditorState }> = ({ message }) => {
   return (
     <Width className="my-12" width="100">
-      {message && <RichText data={message} />}
+      {/* {message && <RichText data={message} />} */}
+      <p>message</p>
     </Width>
   )
 }
